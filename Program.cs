ac47909ed947c0dc60220836adf374a27a92caac
AnaComBia <anabeatrizetorres@gmail.com>
2021-11-27 20:07:47 -0300

    finaliza programa Media Decimal

--- Program.cs
+++ Program.cs
@@ -11,7 +11,12 @@ namespace MediaDecimal
             Console.Write("Digite a quantidade de números que você deseja: ");
             int quantidadeAparece = Convert.ToInt32(Console.ReadLine());
 
-            decimal soma, média, maior, menor, NumeroUsuario;
+            decimal soma, media, maior, menor, NumeroUsuario;
+
+            soma = 0;
+            maior = 123.45M;
+            menor = 678.91M;
+            NumeroUsuario = 0;
 
             int inicio = 0;
 
@@ -21,8 +26,39 @@ namespace MediaDecimal
                 Console.Write($"Número #{inicio}: ");
                 NumeroUsuario = Convert.ToDecimal(Console.ReadLine());
 
+                if (inicio == 1)
+                {
+                    maior = NumeroUsuario;
+                    menor = NumeroUsuario;
+                }
+                else if (NumeroUsuario > maior)
+                {
+                    maior = NumeroUsuario;
+                }
+                else if (NumeroUsuario < menor)
+                {
+                    menor = NumeroUsuario;
+                }
+
             }
-           
+
+            if (NumeroUsuario < 0)
+           {
+               Console.WriteLine($"o número {NumeroUsuario}, não é válido");
+           }
+           else
+           {
+               media = NumeroUsuario / quantidadeAparece;
+               Console.WriteLine($"Média = {media}");
+
+               soma = soma + NumeroUsuario;
+               Console.WriteLine($"Soma = {soma}");
+
+               Console.WriteLine($"Maior Número = {maior}");
+               Console.WriteLine($"Menor Número = {menor}");
+
+
+           }
             
             
 
