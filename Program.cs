b9ef06b0119b391d41591eb5748d101d21ae5e40
AnaComBia <anabeatrizetorres@gmail.com>
2021-11-26 21:53:53 -0300

    finaliza programa MediaDecimal

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MediaDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---Média Decimal---\n");
+
+            Console.Write("Digite a quantidade de números que você deseja: ");
+            int quantidade = Convert.ToInt32(console.ReadLine());
+
+            decimal soma, média, maior, menor, NumeroUsuario;
+
+            int inicio = 0;
+
+            if (quantidade > 0)
+            {
+
+                while (inicio == quantidade)
+                {
+                    inicio += 1 
+                    Console.WriteLine($"Número #{inicio}");
+                    decimal NumeroUsuario = Convert.ToDecimal(Console.ReadLine);
+                }
+
+            }
+            
+        }
+    }
+}
